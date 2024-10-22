6427645ac7d879dd660bd6b5769a21b6f2951bca
Keidson Roby <keidsondesigner@gmail.com>
2024-10-21 20:22:34 -0400

    feat: passando o recurso "users" no Controller

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -9,19 +9,18 @@ type User = {
 }
 
 
-//@Controller('users')
-@Controller()
+@Controller('users')
 export class UserController {
   constructor() {}
 
   // http://localhost:3000/users/2/pedro
-  @Get('/users/:id/:name')
+  @Get('/:id/:name')
   findById(@Param("id") id: string, @Param("name") name: string) {
     return `Usuario encontrado por Id: ${JSON.stringify(id)} e Nome: ${JSON.stringify(name)}`;
   }
 
   // http://localhost:3000/users/findAllByPage?p=10&r=1000
-  @Get('/users/findAllByPage')
+  @Get('/findAllByPage')
   findAllByPage(@Query("p") page: number, @Query("r") limit: number) {
     return `Retornando ${JSON.stringify(page)} de ${JSON.stringify(limit)}`;
   }
@@ -32,7 +31,7 @@ export class UserController {
   //   "email": "teste@email.com",
   //   "password": "12345678"
   // }
-  @Post('/users/create')
+  @Post('/create')
   create(@Body() body: User) {
     return {
       ...body,
