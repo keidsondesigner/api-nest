5ee2c6c6ffdc6e508d548462f995ed6b9b2b2477
Keidson Roby <keidsondesigner@gmail.com>
2024-10-21 23:48:17 -0400

    feat: Implementado Interface IUserRepository na class UserImplRepository

---
+++ src/modules/users/repositories/user-impl.repository.ts
@@ -0,0 +1,29 @@
+import { PrismaService } from "src/infra/database/prisma.service";
+import { UserNameAndEmailDTO, CreatedUserDTO, UserDTO } from "../dto/user.dto";
+import { IUserRepository } from "./user.repository";
+
+export class UserImplRepository  implements IUserRepository{
+
+  constructor(private prismaService: PrismaService) {}
+
+  async findByUserOrEmail(data: UserNameAndEmailDTO): Promise<CreatedUserDTO | null> {
+    // verificando se o User existe;
+    return await this.prismaService.user.findFirst({
+      where: {
+        OR: [{ name: data.name }, { email: data.email }]
+      }
+    });
+  }
+
+  async save(data: UserDTO): Promise<CreatedUserDTO> {
+    return await this.prismaService.user.create({
+      data: {
+        name: data.name,
+        username: data.username,
+        email: data.email,
+        password: data.password
+      }
+    })
+  }
+
+}
