7d147c88b5d1174e7392f38a236f1ee768e3b999
Keidson Roby <keidsondesigner@gmail.com>
2024-10-21 22:16:31 -0400

    feat(Users): Implementado  CreateUserService no UserController

--- src/modules/users/controllers/user.controller.ts
+++ src/modules/users/controllers/user.controller.ts
@@ -1,42 +1,15 @@
-import { Body, Controller, Get, Param, Post, Query } from "@nestjs/common";
-import { randomUUID } from "crypto";
-
-type User = {
-  id: string;
-  name: string;
-  username: string;
-  email: string;
-  password: string;
-}
+import { CreateUserService } from './../services/create-user.service';
+import { Body, Controller, Post } from "@nestjs/common";
+import { UserDTO } from '../dto/user.dto';
 
 
-@Controller('users')
+@Controller('/users')
 export class UserController {
-  constructor() {}
 
-  // http://localhost:3000/users/2/pedro
-  @Get('/:id/:name')
-  findById(@Param("id") id: string, @Param("name") name: string) {
-    return `Usuario encontrado por Id: ${JSON.stringify(id)} e Nome: ${JSON.stringify(name)}`;
-  }
+  constructor(private createUserService: CreateUserService) {}
 
-  // http://localhost:3000/users/findAllByPage?p=10&r=1000
-  @Get('/findAllByPage')
-  findAllByPage(@Query("p") page: number, @Query("r") limit: number) {
-    return `Retornando ${JSON.stringify(page)} de ${JSON.stringify(limit)}`;
-  }
-
-  // http://localhost:3000/users/create
-  // {
-  //   "name": "teste nome",
-  //   "email": "teste@email.com",
-  //   "password": "12345678"
-  // }
-  @Post('/create')
-  create(@Body() body: User) {
-    return {
-      ...body,
-      id: randomUUID()
-    }
+  @Post()
+  async create(@Body() body: UserDTO) {
+    return await this.createUserService.execute(body);
   }
 }
