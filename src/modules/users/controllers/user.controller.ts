f480356303c33a59b2703c85fb0b02748f439897
Keidson Roby <keidsondesigner@gmail.com>
2024-10-21 21:43:37 -0400

    chore: movido UserController para pasta "Controllers"

---
+++ src/modules/users/controllers/user.controller.ts
@@ -0,0 +1,42 @@
+import { Body, Controller, Get, Param, Post, Query } from "@nestjs/common";
+import { randomUUID } from "crypto";
+
+type User = {
+  id: string;
+  name: string;
+  username: string;
+  email: string;
+  password: string;
+}
+
+
+@Controller('users')
+export class UserController {
+  constructor() {}
+
+  // http://localhost:3000/users/2/pedro
+  @Get('/:id/:name')
+  findById(@Param("id") id: string, @Param("name") name: string) {
+    return `Usuario encontrado por Id: ${JSON.stringify(id)} e Nome: ${JSON.stringify(name)}`;
+  }
+
+  // http://localhost:3000/users/findAllByPage?p=10&r=1000
+  @Get('/findAllByPage')
+  findAllByPage(@Query("p") page: number, @Query("r") limit: number) {
+    return `Retornando ${JSON.stringify(page)} de ${JSON.stringify(limit)}`;
+  }
+
+  // http://localhost:3000/users/create
+  // {
+  //   "name": "teste nome",
+  //   "email": "teste@email.com",
+  //   "password": "12345678"
+  // }
+  @Post('/create')
+  create(@Body() body: User) {
+    return {
+      ...body,
+      id: randomUUID()
+    }
+  }
+}
