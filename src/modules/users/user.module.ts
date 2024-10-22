a8b64cd0a527552c35ea215c80e82d0adca59c7c
Keidson Roby <keidsondesigner@gmail.com>
2024-10-21 22:17:18 -0400

    feat(Users): Provendo CreateUserService, PrismaService para o Modulo User

--- src/modules/users/user.module.ts
+++ src/modules/users/user.module.ts
@@ -1,10 +1,12 @@
 import { Module } from "@nestjs/common";
 import { UserController } from "./controllers/user.controller";
+import { CreateUserService } from "./services/create-user.service";
+import { PrismaService } from "src/infra/database/prisma.service";
 
 @Module({
   imports: [],
   controllers: [UserController],
-  providers: [],
+  providers: [CreateUserService, PrismaService],
 })
 export class UserModule {
 
