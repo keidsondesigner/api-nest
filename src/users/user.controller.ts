b70ea0243e05f6cf0fa674104649291881a8a231
Keidson Roby <keidsondesigner@gmail.com>
2024-10-21 20:00:31 -0400

    feat: Métodos com @Params

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Param } from "@nestjs/common";
 
 //@Controller('users')
 @Controller()
 export class UserController {
   constructor() {}
 
-  @Get('/helloUser')
-  helloUser() {
-    return 'hello user';
+  // http://localhost:3000/users/2/pedro
+  @Get('/users/:id/:name')
+  findById(@Param("id") id: string, @Param("name") name: string) {
+    return `Usuario encontrado por Id: ${JSON.stringify(id)} e Nome: ${JSON.stringify(name)}`;
   }
 }
